4ca80df7266b499c1a4f0244d0065f1568448687
Bruno Henry <henrydecarvalho@hotmail.com>
2022-09-28 01:48:51 -0300

    removed unecessary else

--- exercise_1.js
+++ exercise_1.js
@@ -23,8 +23,8 @@ const isDivisibleBy5 = value => value % 5 === 0;
 const isDivisibleBy3And5 = value => isDivisibleBy3(value) && isDivisibleBy5(value);
 const getEvaluatedResult = value => {
 	if (isDivisibleBy3And5(value)) return 'Visual Nuts';
-	else if (isDivisibleBy3(value)) return 'Visual';
-	else if (isDivisibleBy5(value)) return 'Nuts';
+	if (isDivisibleBy3(value)) return 'Visual';
+	if (isDivisibleBy5(value)) return 'Nuts';
 	
 	return value;
 };
